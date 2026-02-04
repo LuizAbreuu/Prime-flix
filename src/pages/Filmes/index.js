b5089e609f9b955924b8ee034a9ad109f7bd243a
LuizAbreuu <luiz_mullercosta@yahoo.com.br>
2026-02-03 22:35:48 -0300

    desenvolvendo a parte 2 dos detalhes filme

--- src/pages/Filmes/index.js
+++ src/pages/Filmes/index.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import './filme-info.css';
 
 import api from '../../services/api';
 
 function Filmes(){
     const {id} = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function loadFilme() {
@@ -14,15 +17,50 @@ function Filmes(){
                     language: "pt-BR",
                 }
             })
+            .then((response) => {
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(() => {
+                
+            })
+        }
+
+        loadFilme();
+
+        return() => {
+            console.log("Componente desmontado");
         }
 
     }, [])
 
+    if(loading){
+        return(
+            <div className="filme-info">
+                <h1>Carregando detalhes....</h1>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>
-                Bem vindo a Pagina de sobre Filmes;
-            </h1>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+            
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href="#">
+                        Trailer
+                    </a>
+                </button>
+
+            </div>
         </div>
     )
 }
